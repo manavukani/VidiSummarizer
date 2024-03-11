96897ce1bebe2256568b56768b6cc1068d70c9b5
Manav Ukani <84531789+manavukani@users.noreply.github.com>
2024-03-11 17:13:17 +0530

    fixed url with playlist id

--- extract_transcript.py
+++ extract_transcript.py
@@ -1,9 +1,16 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
-
 def extract_transcript(youtube_video_url):
     try:
-        video_id = youtube_video_url.split("=")[-1]
+
+        # for playist
+        # sample url : https://www.youtube.com/watch?v=OmS94lV-X6o&list=WL&index=44
+        video_id = ""
+        if "&" in youtube_video_url:
+            video_id = youtube_video_url.split("v=")[1].split("&")[0]
+        else:
+            video_id = youtube_video_url.split("=")[1]
+
         transcript_text = YouTubeTranscriptApi.get_transcript(video_id)
         transcript = ""
         for i in transcript_text:
